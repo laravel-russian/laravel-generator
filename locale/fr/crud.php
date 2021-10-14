51598fdd4e24aadc00c41e6268234d3f76919d85
Guillaume Ernst <guillaume.ernst@infernal.media>
2021-10-14 16:04:03 -0400

    feat(locale): add french files

---
+++ locale/fr/crud.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'add_new' => 'Ajouter',
+    'cancel' => 'Annuler',
+    'save' => 'Sauvegarder',
+    'delete' => 'Supprimer',
+    'send' => 'Envoyer',
+    'detail' => 'Détail',
+    'back' => 'Retour',
+    'action' => 'Action',
+    'id' => 'Id',
+    'created_at' => 'Créé le',
+    'updated_at' => 'Mis à jour le',
+    'deleted_at' => 'Supprimé le',
+    'are_you_sure' => 'Êtes-vous certain?',
+];
