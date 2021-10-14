5df915cb5285654829c99fee4941608217fd1ccb
Guillaume Ernst <guillaume.ernst@infernal.media>
2021-10-14 16:04:03 -0400

    feat(locale): add french files

---
+++ locale/fr/messages.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'saved' => ':Model créé(e) avec succès.',
+    'saved-gender' => [
+        'male' => ':Model créé avec succès.',
+        'female' => ':Model créée avec succès.',
+    ],
+    'updated' => ':Model mis(e) à jour avec succès.',
+    'updated-gender' => [
+        'male' => ':Model mis à jour avec succès.',
+        'female' => ':Model mise à jour avec succès.',
+    ],
+    'password_updated' => 'Mot de passe mis à jour avec succès.',
+    'deleted' => ':Model supprimé(e) avec succès.',
+    'deleted-gender' => [
+        'male' => ':Model supprimé avec succès.',
+        'female' => ':Model supprimée avec succès.',
+    ],
+    'not_found' => ':Model introuvable',
+    'forbidden' => "Vous n'êtes pas autorisé à modifier cet élément.",
+];
