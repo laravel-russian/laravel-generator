96ac4b0b7d0a97eb52a56636c04aa726308a56d4
Guillaume Ernst <guillaume.ernst@infernal.media>
2021-10-14 15:54:50 -0400

    feat(locale): update text messages

--- locale/en/messages.php
+++ locale/en/messages.php
@@ -1,11 +1,23 @@
 <?php
 
 return [
-
     'retrieved' => ':model retrieved successfully.',
-    'saved'     => ':model saved successfully.',
-    'updated'   => ':model updated successfully.',
-    'deleted'   => ':model deleted successfully.',
-    'not_found' => ':model not found',
-
+    'saved' => ':Model successfully created.',
+    'saved-gender' => [
+        'male' => ':Model successfully created',
+        'female' => ':Model successfully created',
+    ],
+    'updated' => ':Model successfully updated.',
+    'updated-gender' => [
+        'male' => ':Model successfully updated.',
+        'female' => ':Model successfully updated.',
+    ],
+    'password_updated' => 'Password successfully updated.',
+    'deleted' => ':Model successfully deleted.',
+    'deleted-gender' => [
+        'male' => ':Model successfully deleted.',
+        'female' => ':Model successfully deleted.',
+    ],
+    'not_found' => ':Model not found',
+    'forbidden' => 'You are not allowed to modify this :model.',
 ];
