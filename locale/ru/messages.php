8e0d4f73bbcb660ba613a528b2aa2f3fbaa81212
Mike <mltefive@gmail.com>
2022-06-19 02:57:01 +0900

    add RU lang and $API_VERSION$ to route

---
+++ locale/ru/messages.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'retrieved' => ':model retrieved successfully.',
+    'saved' => ':Model successfully created.',
+    'saved-gender' => [
+        'male' => ':Model successfully created',
+        'female' => ':Model successfully created',
+    ],
+    'updated' => ':Model successfully updated.',
+    'updated-gender' => [
+        'male' => ':Model successfully updated.',
+        'female' => ':Model successfully updated.',
+    ],
+    'password_updated' => 'Password successfully updated.',
+    'deleted' => ':Model successfully deleted.',
+    'deleted-gender' => [
+        'male' => ':Model successfully deleted.',
+        'female' => ':Model successfully deleted.',
+    ],
+    'not_found' => ':Model not found',
+    'forbidden' => 'You are not allowed to modify this :model.',
+];
